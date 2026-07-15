b70c1ebd522b2dce11ff933c59262adb00ef3003
marcelo-de-santana <marcelondesantana@outlook.com>
2026-07-15 01:46:32 -0300

    feat: adicionando comando personalizado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,7 @@
 Cypress.Commands.add('getByTestData', (selector) => {
   return cy.get(`[data-test=${selector}]`);
 });
+
+Cypress.Commands.add('getBy', (selector, text) => {
+  cy.get(selector).contains(text);
+});
