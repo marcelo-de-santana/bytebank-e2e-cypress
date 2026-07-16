a1f0d4c0e98b12a47c2a81e76c953bfc3286d3f2
marcelo-de-santana <marcelondesantana@outlook.com>
2026-07-16 00:22:06 -0300

    feat: validando mensagem de erro de e-mail e senha

--- cypress/e2e/formularioLogin.spec.cy.js
+++ cypress/e2e/formularioLogin.spec.cy.js
@@ -12,4 +12,22 @@ describe('Formulário de Login', () => {
       .should('exist')
       .and('have.text', 'O email digitado é inválido');
   });
+
+  it('Não deve permitir um campo em branco', () => {
+    cy.getByTestData('botao-login').click();
+    cy.getByTestData('senha-input').type('123456');
+    cy.getByTestData('botao-enviar').click();
+    cy.getByTestData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório');
+  });
+
+  it('Não deve permitir uma senha em branco', () => {
+    cy.getByTestData('botao-login').click();
+    cy.getByTestData('email-input').type('marcelo@alura');
+    cy.getByTestData('botao-enviar').click();
+    cy.getByTestData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo de senha é obrigatório');
+  });
 });
